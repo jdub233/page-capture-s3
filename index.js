e9b00e8661b330e065c9280acaa380a994ab8dcc
Jonathan Williams <tamlin@mac.com>
2019-05-07 15:03:09 -0400

    add s3 upload

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
 const scrape = require('website-scraper');
+const s3 = require('s3-node-client');
+const fs = require('fs-extra');
+
 require('dotenv').config();
 
-const options = {
+const scrapeOptions = {
     urls: [process.env.CAPTURE_URL],
     directory: './data/page/',
 };
 
+const client = s3.createClient();
+
+const uploadParams = {
+    localDir: 'data/page',
+    deleteRemoved: true,
+    s3Params: {
+        Bucket: process.env.S3_BUCKET_NAME,
+        Prefix: process.env.S3_PATH,
+    },
+};
+
 (async () => {
-    const result = await scrape(options);
-    console.log(result);
+    const result = await scrape(scrapeOptions);
+    console.log('capture complete');
+    if (result[0].saved) {
+        //If successful, upload files
+        console.log('uploading');
+        const uploader = client.uploadDir(uploadParams);
+
+        uploader.on('error', function(err) {
+            console.error("unable to sync:", err.stack);
+        });
+        uploader.on('end', function() {
+            console.log("done uploading, removing local files");
+            fs.remove('./data/page/', err =>{
+                if (err) {
+                    return console.error(err);
+                }
+                console.log('success, local files removed');
+            });
+
+        });
+    };
+
 })();
