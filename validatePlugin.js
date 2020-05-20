f6a1eb33ece47f021940a6b6f0852ba6356e3fb6
Jonathan Williams <tamlin@mac.com>
2020-05-20 15:48:48 -0400

    adjust error message

--- validatePlugin.js
+++ validatePlugin.js
@@ -5,7 +5,7 @@ module.exports = class ValidatePlugin {
         registerAction('afterResponse', async ({response}) => {   
             if (response.statusCode !== 200) {
                 // Don't capture bad assets.  Also cancels the upload phase if the root page has a bad status code.
-                console.log( `Bad status code ${response.statusCode} , cancelling asset capture` );
+                console.log( `A bad status code ${response.statusCode} was encountered, cancelling asset capture` );
                 return null;
             } else {
                 return {
